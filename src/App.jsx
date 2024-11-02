e056f507327769a39dc4a968618fd90e703f344f
uzair73 <uzairhakeem71@gmail.com>
2024-11-02 20:56:19 +0500

    add the main weather details comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css'
 import ToggleButton from './component/header/ToggleButton'
 import Display_time from './component/header/display_time/Display_time';
+import Weather_details from './component/header/weather-details/Weather_details';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,7 +14,10 @@ function App() {
     <>
     <div className="container w-[100vw] h-[100vh] mx-10 body_color">
       <ToggleButton onToggleTheme={toggleTheme} isDarkMode={isDarkMode}/>
+      <div className='flex justify-between'>
       <Display_time/>
+      <Weather_details/>
+      </div>
     </div>
     </>
   )
