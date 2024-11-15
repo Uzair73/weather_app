c5f1b271303216e1872cec6620dae3c2846314e6
uzair73 <uzairhakeem71@gmail.com>
2024-11-15 12:27:45 +0500

    update the apis route

--- src/api/weather_api.js
+++ src/api/weather_api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-
+const host = 'http://localhost:3000'
 export const current_weather_api =  async (city)=>{
-const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_CURRENT_WEATHER_API}`
+const url =  `${host}/weather-forcast?city=${city}&units=metric`
 const res = await axios.get(url)
 return res
 }
 
 export const days_weather_data = async (lat, lon) =>{
- const res  = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_CURRENT_WEATHER_API}`)
+ const res  = await axios.get(`${host}/hourly-forcast?lat=${lat}&lon=${lon}`)
  const data = res
 //  console.log("data of days forcast>>", data);
  return data
